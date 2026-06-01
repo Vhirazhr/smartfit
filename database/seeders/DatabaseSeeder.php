8790d21eb3af432c4e9846c8499aeada92b6be3c
Amilil <amililuing86@gmail.com>
2026-06-01 21:00:47 +0700

    feat: Add FashionRecommendationPhotoSeeder to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             FashionGallerySeeder::class,
+            FashionRecommendationPhotoSeeder::class,
             BodyMeasurementAttemptSeeder::class,
         ]);
     }
